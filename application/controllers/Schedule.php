b7dfd3d66a914546396026aaa21eb1e74a5d328e
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2015-12-04 18:30:54 +0700

    Schedule add beta #1

--- application/controllers/Schedule.php
+++ application/controllers/Schedule.php
@@ -1,9 +1,10 @@
 <?php
 
-class Schedule extends CI_Controller{
+class Schedule extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+        $this->load->model('Schedule_model');
         $this->load->model('Bus_model');
     }
 
@@ -17,8 +18,7 @@ class Schedule extends CI_Controller{
      * @route: /schedule
      */
     public function index() {
-        if (1 != $this->session->userdata('type'))
-            show_error('401 Unauthorized Request', 401 );
+        if (1 != $this->session->userdata('type')) show_error('401 Unauthorized Request', 401);
 
         $this->load->view('schedules/schedule-all');
     }
@@ -28,43 +28,57 @@ class Schedule extends CI_Controller{
      *
      * @target: Admin
      * @method: POST, GET
-     * @route: /schedule/add  
+     * @route: /schedule/add
      */
     public function add() {
-        date_default_timezone_set('Asia/Jakarta');
 
-        /**
-         * monday-1 senin, tuesday-2 selasa, dst...
-         * Jika input post hari adalah tuesday-2 maka akan
-         * di explode menjadi [0] => 'tuesday', [1] => 2
-         */
-        $schedule_day = explode('-', 'tuesday-2');
-
-        echo $schedule_day[0] . '<br>';
-
-        $startDate = strtotime("next {$schedule_day[0]}");
-
-        echo date('d-m-Y', $startDate ) . "<br>";
-
-        $i=0;
-        for ($i; $i < 11; $i++) { 
-            echo date('d-m-Y', strtotime('+1 week', $startDate ) ) . "<br>";
-            $startDate = strtotime('+1 week', $startDate );
+        if ("POST" === $this->input->server('REQUEST_METHOD')) {
+            # set zona waktu lokal
+            date_default_timezone_set('Asia/Jakarta');
+            # ambil !D bus
+            $bus = substr($this->input->post('bus'), strpos(
+                    $this->input->post('bus'), ':'
+                ) + 2, -1
+            );
+            /**
+             * monday-1 senin, tuesday-2 selasa, dst...
+             * Jika input post hari adalah tuesday-2 maka akan
+             * di explode menjadi [0] => 'tuesday', [1] => 2
+             */
+            $schedule_day = explode('-', $this->input->post('day'));
+            $schedule_time = date('H:i:s', strtotime($this->input->post('time')));
+            $startDateSource = strtotime("next {$schedule_day[0]}");
+            $startDate = date('Y-m-d', $startDateSource);
+            # simpan 1 jadwal pertama dalam database
+            $this->Schedule_model->create(3, "{$startDate} {$schedule_time}");
+            $dateNext = $startDateSource;
+            $i = 0;
+            for (; $i < 11; $i++) {
+                # simpan 11 jadwal sisanya dalam database
+                $this->Schedule_model->create($bus, date('Y-m-d', strtotime('+1 week', $dateNext)) . ' ' . $schedule_time);
+                $dateNext = strtotime('+1 week', $dateNext);
+            }
+            # simpan info dalam session
+            $this->session->set_userdata('flash-message', 'Jadwal telah ditambahkan!');
+            # arahkan kembali ke /account
+            redirect(base_url('account'), 'refresh');
+        } else {
+            $buses = $this->Bus_model->getAll();
+            $this->load->view('schedules/add-schedule', ['buses' => $buses]);
         }
+
     }
 
     public function search() {
-        
+
     }
 
 
     public function edit() {
-        if (1 != $this->session->userdata('type'))
-            show_error('401 Unauthorized Request', 401 );
+        if (1 != $this->session->userdata('type')) show_error('401 Unauthorized Request', 401);
     }
 
     public function delete() {
-        if (1 != $this->session->userdata('type'))
-            show_error('401 Unauthorized Request', 401 );
+        if (1 != $this->session->userdata('type')) show_error('401 Unauthorized Request', 401);
     }
 }
--- application/models/Schedule_model.php
+++ application/models/Schedule_model.php
@@ -2,12 +2,21 @@
 
 class Schedule_model extends CI_Model {
 
+    public function __construct() {
+        parent::__construct();
+    }
+
     public function getAll() {
         
     }
 
-    public function create() {
-        
+    public function create($bus_id, $dateTime) {
+        $data = array(
+            'bus_id' => $bus_id,
+            'time' => $dateTime
+        );
+
+        $this->db->insert('schedule', $data);
     }
 
     public function getByBusname() {
